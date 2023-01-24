c868057ace92cbaae8eeaea6579f55c52ec5ec87
ko0000 <hongyibo0219@gmail.com>
2023-01-24 11:00:36 -0800

    0124

--- Module1-Intro_to_Python/day4.py
+++ Module1-Intro_to_Python/day4.py
@@ -1,3 +1,8 @@
 from sys import argv
 name, age, height, weight = argv 
-print(f"i am {age} years old, {height} cm tall, and {weight} lbs weight.") 
+print(f"""i am {age} years old, 
+{height} cm tall, 
+and {weight} lbs weight.""") 
+
+
+
